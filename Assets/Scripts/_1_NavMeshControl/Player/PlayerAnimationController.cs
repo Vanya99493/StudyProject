c3a162bf109eed56a24f6ad54245e3d8c4492dc5
bowlofruit <76440759+bowlofruit@users.noreply.github.com>
2024-09-06 16:54:19 +0300

    Remove backwards run from animation blend tree

--- Assets/Scripts/_1_NavMeshControl/Player/PlayerAnimationController.cs
+++ Assets/Scripts/_1_NavMeshControl/Player/PlayerAnimationController.cs
@@ -4,9 +4,6 @@ namespace _1_NavMeshControl.Player
 {
 	public class PlayerAnimationController : MonoBehaviour
 	{
-		private const string ANIMATOR_TURN_STATE_NAME = "Turn";
-		
-		private const string ANIMATOR_TURN_NAME = "TurnAngle";
 		private const string ANIMATOR_HORIZONTAL_SPEED_NAME = "HorizontalSpeed";
 		private const string ANIMATOR_VERTICAL_SPEED_NAME = "VerticalSpeed";
 		
@@ -26,16 +23,6 @@ namespace _1_NavMeshControl.Player
 		public void SetVerticalSpeed(float newVerticalSpeed, float dampTime)
 		{
 			_animator.SetFloat(ANIMATOR_VERTICAL_SPEED_NAME, newVerticalSpeed, dampTime, Time.deltaTime);
-		}
-		
-		public void SetTurnAngle(float newTurnAngle)
-		{
-			_animator.SetFloat(ANIMATOR_TURN_NAME, newTurnAngle);
-		}
-
-		public void EntryTurnState()
-		{
-			_animator.Play(ANIMATOR_TURN_STATE_NAME);
 		}
 	}
 }
