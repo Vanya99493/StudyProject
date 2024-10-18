3cb395cf70f9652b9bb797a39f497c9c9d2a9dbb
Vanya99493 <vanyazadorozhnii99493@gmail.com>
2024-10-18 09:36:06 +0300

    feat: add fog enabler on water

---
+++ Assets/Scripts/_3D-FP/Environment/FogSpace.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace StudyProj.Environment
+{
+	public class FogSpace : MonoBehaviour
+	{
+		[SerializeField] private LayerMask _interactiveLayerMask;
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if ((_interactiveLayerMask.value & (1 << other.gameObject.layer)) != 0)
+			{
+				RenderSettings.fog = true;
+			}
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			if ((_interactiveLayerMask.value & (1 << other.gameObject.layer)) != 0)
+			{
+				RenderSettings.fog = false;
+			}
+		}
+	}
+}
