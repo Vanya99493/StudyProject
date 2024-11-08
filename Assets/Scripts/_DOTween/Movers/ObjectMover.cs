df1743866b5bf872128eeba9f4efb9bd2d1ef358
Vanya99493 <vanyazadorozhnii99493@gmail.com>
2024-11-08 10:51:00 +0200

    feat: add props; add DOTween with sample realization

---
+++ Assets/Scripts/_DOTween/Movers/ObjectMover.cs
@@ -0,0 +1,49 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace _DOTween.Movers
+{
+	public class ObjectMover : MonoBehaviour
+	{
+		[SerializeField] private float _movementDuration;
+		[SerializeField] private float _rotationDuration;
+		[SerializeField] private bool _isPlaying = false;
+
+		private Sequence _movingSequence;
+
+		private void Start()
+		{
+			StartMoving();
+		}
+
+		private void Update()
+		{
+			HandleSequencePause();
+		}
+
+		private void HandleSequencePause()
+		{
+			if (_isPlaying && !_movingSequence.IsPlaying())
+			{
+				_movingSequence?.Play();
+			}
+			else if (!_isPlaying && _movingSequence.IsPlaying())
+			{
+				_movingSequence?.Pause();
+			}
+		}
+
+		private void StartMoving()
+		{
+			_movingSequence?.Kill();
+			_movingSequence = DOTween.Sequence()
+				.Pause()
+				.Append(transform.DOMove(transform.position + transform.forward, _movementDuration))
+				.AppendInterval(1f)
+				.Append(transform.DORotate(transform.eulerAngles + new Vector3(0f, 90f, 0f), _rotationDuration))
+				.AppendInterval(1f)
+				.SetLink(gameObject, LinkBehaviour.KillOnDestroy)
+				.OnComplete(StartMoving);
+		}
+	}
+}
